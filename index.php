7fa23bc40d186d6958f033329c31bc5196a79a89
Gulbis <rgubis41@gmail.com>
2024-11-26 15:10:14 +0200

    leons ir sigma

--- index.php
+++ index.php
@@ -1,9 +1,41 @@
 <?php
+
 $connection = new mysqli("localhost", "root", "", "stars");
 $result = $connection->query("SELECT * FROM users");
 $data = $result->fetch_assoc();
-print_r($data);
 
-echo '<form>';
+echo '<style>
+    body {
+    background-color: #28282B;
+    color: rgb(0, 255, 255);
 
+    input {
+    border-radius: 3px;
+    border-color: rgb(0, 255, 255);
+    border-style: solid;
+    background-color: #3f3f41;
+    color: lightgray;
+    margin-top: 3px;
+}
+}
+</style>';
+
+echo "<pre>";
+print_r($_SERVER);
+echo "</pre>";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST["username"];
+    $password = $_POST["pass"];
+
+    $insert = "INSERT INTO users (username, pass)
+    VALUES ('$username', '$password')";
+
+    $connection->query($insert);
+}
+
+echo '<form method="POST">';
+    echo "<label>Username: <input placeholder='Username' required name='username'/></label><br>";   
+    echo "<label>Password: <input type='password' placeholder='Password' required name='pass'/></label><br>";
+    echo "<input type='submit' value='Reģistreties'/>";
 echo '</form>';
---
+++ script.js
@@ -0,0 +1,6 @@
+let x = 0;
+
+function clicker() {
+    x++;
+    document.getElementById("yes").innerHTML = x;
+}
